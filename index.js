e17201b2ed443431b83c466c2200dcb5f3545e0f
vanumdaniel7 <vanumdaniel7@gmail.com>
2023-04-18 00:08:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const path = require("path");
 const express = require("express");
+const db = require("./db/postgresql.js");
 const authRoutes = require("./routes/authRoutes.js");
 const blogRoutes = require("./routes/blogRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
-const db = require("./db/postgresql.js");
-require("dotenv").config();
 const app = express();
 
 const startApp = async () => {
@@ -14,4 +14,6 @@ app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/blogs", blogRoutes);
 app.use("/api/v1/users", userRoutes);
+app.use(express.static(path.join(__dirname, "./client/build/")));
+app.get("*", (req, res) => { res.sendFile(path.join(__dirname, "./client/build/index.html"))});
 startApp();
